1689244138af48f012e24634cabeeb68f3f12c35
Rishika Gandhi <gandhirishika7@gmail.com>
2023-07-02 16:45:14 +0530

    struct,union

--- tut12.cpp
+++ tut12.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 int main()
 {
 
+//pointers
 
     int a = 3;
     int *b = &a;
@@ -10,5 +12,46 @@ int main()
     cout<<b<<endl;
     cout<<*b<<endl;
     return 0;
+
+    //Arrays and Pointers
+
+    int arr[4]={11,2,3,4};
+    int *p =arr;
+    cout<<*p<<endl;
+    cout<<*(p+1)<<endl;
+    cout<<*(p+2)<<endl;
+    cout<<*(p+3)<<endl;
+
+
+
+    //structures
+    struct emp{
+        string name,
+        float salary,
+        int age,
+    }
+    
+
+    union money{ //if u use only one type of currency then use union as it take less memory by allocating maximum bytes
+        int INR;
+        float Pounds,
+
+    }
+
+
+    struct emp rishika;
+    struct emp sonu;
+    struct emp harsh;
+
+    cout<<harsh.name<<endl
+
+
+
+    {
+        /* data */
+    };
+    
+
+    
 
 }
